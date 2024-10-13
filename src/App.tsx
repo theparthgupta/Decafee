c12912a6952f051a9b41c12ee8b7abb149efcea5
unknown <parth.gupt07@gmail.com>
2024-10-14 03:21:01 +0530

    Replaced all files with the new Decafee project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// src/App.tsx
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Booking from "./pages/Booking";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/booking" element={<Booking />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Welcome to Decafe!</h1>
+      <p>Your ultimate destination for finding the perfect café or restaurant.</p>
+      {/* Add more content here */}
+    </div>
+  );
+};
+
+export default Home;
